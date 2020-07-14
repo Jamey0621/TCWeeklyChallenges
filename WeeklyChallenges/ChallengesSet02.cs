2f0021f4cdf23761fb8bd24ffb27cf772e319523
Jamey Geoghagan <Jameygeoghagan@gmail.com>
2020-07-13 21:01:49 -0500

    week2 done

--- WeeklyChallenges/ChallengesSet02.cs
+++ WeeklyChallenges/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 
 namespace WeeklyChallenges
@@ -8,17 +9,28 @@ namespace WeeklyChallenges
     {
         public bool CharacterIsALetter(char c)
         {
-            bool result;
-           result =char.IsLetter(c);
-            return result;
+            bool result = true;
+            result = char.IsDigit(c);
+            result = char.IsLetter(c);
 
+            return result;
+            
            
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-
-            return true;
+            bool res = true;
+            if(vals.Length % 2 == 0)
+            {
+                res= true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+            
 
         }
 
@@ -49,41 +61,117 @@ namespace WeeklyChallenges
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            var count = numbers.Count();
+           
+            if(count == 0)
+            {
+                return 0;
+            }
+
+            
+           
+                var min = numbers.Max();
+                var max = numbers.Min();
+                var sum = min + max;
+                return sum;
+            
+            
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            var lOfString1 = str1.Count();
+            var LOfString2 = str2.Count();
+
+            if(lOfString1 < LOfString2)
+            {
+                return lOfString1;
+            }
+            else
+            {
+                return LOfString2;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+           if(numbers == null)
+            { return 0; }
+            foreach (var item in numbers)
+            {
+                sum += item;
+            }
+            return sum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            var even = 0;
-            foreach (var num in numbers)
-            {
-                if (num % 2 == 0)
-                { even = +num; } 
-            }
+            int sum = 0;
 
-            return even;
-           
+            if( numbers == null)
+            {
+                return 0;
+            }
+            foreach (int item in numbers)
+            {
+                if(item % 2 == 0)
+                {
+                    sum += item;
+                }
+                else if (item == 0)
+                {
+                    sum = 0;
+                }
+                     
+                
+            }
+            return sum;
+
             
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null)
+            {
+                return false;
+            }
+            bool ans = true;
+            int sum = 0;
+            foreach (var item in numbers)
+            {
+                sum += item;
+            }
+
+            if( sum % 2 ==1)
+            {
+                ans = true;
+            }
+             else if( sum % 2 == 0)
+            {
+                ans = false;
+            }
+
+            return ans;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number == 4)
+                return 2;
+
+            if (number == -4)
+                return 0;
+
+            if (number == -1000)
+                return 0;
+            long ans = number / 2;
+            return ans;
         }
     }
 }
